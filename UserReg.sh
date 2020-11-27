2b2d4395f526b26f61e649018110075a78802dad
rohit001007 <rohit001007@gmail.com>
2020-11-27 13:32:24 +0530

    Added : Password Of User With Condition Minimum 1 Special Character

--- UserReg.sh
+++ UserReg.sh
@@ -3,13 +3,13 @@
 echo -ne "\n"
 read -p "Enter Password : " pass
 
-pat7="^(?=.*[A-Z])[a-z](?=.*\d)\S{8,}$"
+pat8="^(?=.*[A-Z])[a-z](?=.*[!@#$%^&*])(?=.*\d)\S{8,}$"
 
-if [[ $pass =~ $pat7 ]]
+if [[ $pass =~ $pat8 ]]
 then
-	echo -ne "\n $pass Has Minimun 1 Numeric Character \n"
+	echo -ne "\n $pass Has Minimun 1 Special Character \n"
 else
-	echo -ne "\n $pass Has Not Minimun 1 Numeric Character \n"
+	echo -ne "\n $pass Has Not Minimun 1 Special Character \n"
 fi
 
 
