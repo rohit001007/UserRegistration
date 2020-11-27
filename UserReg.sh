ae7b7032606b582e65535127e134df6454007ac2
rohit001007 <rohit001007@gmail.com>
2020-11-27 12:15:02 +0530

    Added : First Name Of User

--- UserReg.sh
+++ UserReg.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-echo "Welcome........!!!"
+echo -ne "\n"
+read -p "Enter First Name : " name
+
+pat="^[A-Z]{1}[a-z]{2,}$"
+
+
+if [[ $name =~ $pat ]]
+then
+	echo -ne "\n $name Is Valid Name \n"
+else
+	echo -ne "\n $name Is Not Valid Name \n"
+fi
+
+
