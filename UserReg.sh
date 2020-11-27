ba32a123c58b294c53ae1b33a152663b58dc4b8a
rohit001007 <rohit001007@gmail.com>
2020-11-27 13:09:11 +0530

    Added : Password Of User With Condition Minimum 1 Upper Case Character

--- UserReg.sh
+++ UserReg.sh
@@ -3,13 +3,13 @@
 echo -ne "\n"
 read -p "Enter Password : " pass
 
-pat5="^.{8,}$"
+pat6="^(?=.*?[[:upper:]]).{8,}$"
 
-if [[ $pass =~ $pat5 ]]
+if [[ $pass =~ $pat6 ]]
 then
-	echo -ne "\n $pass Has Minimun 8 Character \n"
+	echo -ne "\n $pass Has Minimun 1 Upper Case Character \n"
 else
-	echo -ne "\n $pass Has Not Minimun 8 Character \n"
+	echo -ne "\n $pass Has Not Minimun 1 Upper Case Character \n"
 fi
 
 
